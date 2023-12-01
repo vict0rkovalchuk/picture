457806296af4430b5c519e0f6661c329ceb18ee9
Victor <vict02r05@gmail.com>
2023-12-01 19:42:36 +0200

    add calculator to the page

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
+import calc from './modules/calc';
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
@@ -19,4 +20,5 @@ window.addEventListener('DOMContentLoaded', () => {
   checkTextInputs("[name='name']");
   checkTextInputs("[name='message']");
   showMoreStyles('.button-styles', '.styles-2');
+  calc('#size', '#material', '#options', '.promocode', '.calc-price');
 });
