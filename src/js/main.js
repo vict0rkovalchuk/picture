f78ac28c4ddc5cc81dd50a1db821a87bea0df9fb
Victor <vict02r05@gmail.com>
2023-12-02 18:17:12 +0200

    add image changing on sizes-block's hover

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
@@ -23,4 +24,5 @@ window.addEventListener('DOMContentLoaded', () => {
   showMoreStyles('.button-styles', '.styles-2');
   calc('#size', '#material', '#options', '.promocode', '.calc-price');
   filter();
+  pictureSize('.sizes-block');
 });
